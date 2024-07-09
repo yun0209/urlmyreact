6fb6033cea3f1aa315e1d08ba037722c3dddb668
yun0809 <rhgt6002g@gmail.com>
2024-07-09 11:54:09 +0900

    버셀 용

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'swiper/css';
-import 'swiper/css/Pagination'
-import 'swiper/css/Pagination'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
